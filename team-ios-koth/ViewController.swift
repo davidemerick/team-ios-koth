4b683ec4516897bb1c9199476b63e501b6e2c670
Dan Chu <dzjchu@gmail.com>
2017-05-11 14:23:53 -0700

    Changed the project to Dan's initial project form

--- team-ios-koth/ViewController.swift
+++ team-ios-koth/ViewController.swift
@@ -7,20 +7,67 @@
 //
 
 import UIKit
+import CoreLocation
 
 class ViewController: UIViewController {
     
-
+    var locationManager: CLLocationManager?
+    var startLocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        
+        locationManager?.requestWhenInUseAuthorization()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
 
+
+extension ViewController: CLLocationManagerDelegate {
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if startLocation == nil {
+            startLocation = locations.first
+        } else {
+            guard let latest = locations.first else { return }
+            let distanceInMeters = startLocation?.distance(from: latest)
+            print("distance in meters: \(String(describing: distanceInMeters!))")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            locationManager?.startUpdatingLocation()
+            locationManager?.allowsBackgroundLocationUpdates = true
+        }
+    }
+    
+}
+
+
+
+//    
+//    func createFence(startLocation: CLLocation) -> CLCircularRegion {
+//        let fence = CLCircularRegion(
+//            center: startLocation.coordinate,
+//            radius: ourRadius,
+//            identifier: ourString)
+//            
+//        return fence
+//        }
+//
+//    func startMonitoring(for ourFence:() -> CLCircularRegion){
+//        
+//        print (ourFence)
+//        
+//    }
+//}
