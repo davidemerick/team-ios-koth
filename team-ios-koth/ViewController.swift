870d84b0658f74af609145150efd5220532a3ba4
dusty-g <dustygalindo@gmail.com>
2017-05-12 11:53:36 -0700

    got fence working

--- team-ios-koth/ViewController.swift
+++ team-ios-koth/ViewController.swift
@@ -14,39 +14,50 @@ import MapKit
 class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var floorLabel: UILabel!
     @IBOutlet weak var testLabel: UILabel!
+    @IBOutlet weak var enterRegionLabel: UILabel!
+    
     
     
     var locationManager: CLLocationManager = CLLocationManager()
+    let fence: CLCircularRegion = CLCircularRegion(center: CLLocationCoordinate2DMake(47.609930, -122.196759), radius: CLLocationDistance(20), identifier: "coding dojo")
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if locations.first != nil {
             
             let currentLocation = locations.first
+            let currentLocation2D = CLLocationCoordinate2DMake(currentLocation!.coordinate.latitude, currentLocation!.coordinate.longitude)
             
-            if currentLocation?.altitude != nil{
-                
-                testLabel.text = String(currentLocation!.altitude)
-                let color = currentLocation!.altitude / 100
-                
-                
-                testLabel.backgroundColor = UIColor.init(colorLiteralRed: Float(color), green: 0.2, blue: 0.2, alpha: 0.7)
-                
-                
-            }
+
+            testLabel.text =  String(fence.contains(currentLocation2D))
+//            if !fence.contains(currentLocation2D){
+//                    enterRegionLabel.text = "not in region"
+//            }
+            
             
         }
         
+        
     }
+    func locationManager(_ manager: CLLocationManager,
+                                  didEnterRegion region: CLRegion){
+        print("did enter region!")
+        enterRegionLabel.text = "you entered region: \(region.identifier)"
+    }
+    func locationManager(_ manager: CLLocationManager, didStartMonitoringFor region: CLRegion) {
+        enterRegionLabel.text = "you exited region: \(region.identifier)"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        print(fence)
         
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
-        
+        locationManager.startMonitoring(for: fence)
         
         
     }
