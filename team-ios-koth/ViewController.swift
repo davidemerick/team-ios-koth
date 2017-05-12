df65a5943d702b5bdc85db5ab5149aa9fd295cb7
David Emerick <d.emerick@outlook.com>
2017-05-11 21:44:16 -0700

    Don't think I added much, but don't want to lose commented out code.

--- team-ios-koth/ViewController.swift
+++ team-ios-koth/ViewController.swift
@@ -9,27 +9,23 @@
 import UIKit
 import CoreLocation
 
+// Get user location
+// Create fence with user location
+// Check for user inside or outside of fence
+
+// access to UIViewController methods
 class ViewController: UIViewController {
     
     var locationManager: CLLocationManager?
     var startLocation: CLLocation?
+    var testDelegate: CLLocationManagerDelegate?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // setup locationManager
-        setupData()
-        // create instance of CLLocationManager
-        locationManager = CLLocationManager()
+        setupLocationManager()
         
-        locationManager?.delegate = self
-        locationManager?.distanceFilter = kCLLocationAccuracyNearestTenMeters
-        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
         
-        locationManager?.requestWhenInUseAuthorization()
-        
-        //test setupData
-        setupData()
         // location manager set up
         // locationManager.startMonitoring(for region: CLRegion)
     }
@@ -41,8 +37,58 @@ class ViewController: UIViewController {
     
 }
 
-
+// access to CLLocationManagerDelegate methods
 extension ViewController: CLLocationManagerDelegate {
+    
+    /*
+     
+     */
+    func setupLocationManager(){
+
+        // create instance of CLLocationManager
+        locationManager = CLLocationManager()
+        
+        // view controller can now receive the relevant delegate method calls
+        locationManager?.delegate = self
+        
+        // checks user authorization
+        locationManager?.requestWhenInUseAuthorization()
+        
+        //
+        locationManager?.distanceFilter = kCLLocationAccuracyNearestTenMeters
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+
+        
+        // starts generation of updates that report user's current location
+        locationManager?.startUpdatingLocation()
+        
+        // App registers for location updates? (code from iDan: slack)
+        self.locationManager?.allowsBackgroundLocationUpdates = true
+    }
+    
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        let locations = location[0]
+//    }
+    
+    // create fence with location, radius, and test id
+    // return fence
+    func createFence(startLocation: CLLocation, newRadius: CLLocationDistance, fenceID: String) -> CLCircularRegion {
+//        let ourRadius = 10.0
+//        let ourString = "test_id"
+        let fence = CLCircularRegion(
+            center: startLocation.coordinate,
+            radius: newRadius,
+            identifier: fenceID)
+        // registers region in 'monitoredRegions' property
+        locationManager?.startMonitoring(for: fence)
+        return fence
+    }
+    
+    func checkIfInFence(fenceToCheck: CLCircularRegion) {
+        
+    }
+
+
     
     // isMonitoringAvailable, if yes: create region. Then monitor region and print region
     func setupData() {
@@ -75,16 +121,7 @@ extension ViewController: CLLocationManagerDelegate {
         }
     }
     
-    func createFence(startLocation: CLLocation) -> CLCircularRegion {
-        let ourRadius = 10.0
-        let ourString = "test_id"
-        let fence = CLCircularRegion(
-            center: startLocation.coordinate,
-            radius: ourRadius,
-            identifier: ourString)
-        
-        return fence
-    }
+
     
 }
 
